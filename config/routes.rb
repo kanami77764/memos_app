1e588f5b98e8a99c4fdba5ae63d2a04d8e311d04
kanami77764 <64050565+kanami77764@users.noreply.github.com>
2020-05-14 22:48:31 +0900

    user_function1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'memos#index'
+  root to: 'users#index'
+  resources :users, only: [:index, :new, :create, :show]
+  resources :memos, only: [:index, :create, :show, :edit, :update, :destroy]
   
-  resources :memos
   # , only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  
 end
