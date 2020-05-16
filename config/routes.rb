ae5c0aa6261305221fa12e74101f184b373c6781
kanami77764 <64050565+kanami77764@users.noreply.github.com>
2020-05-16 19:03:37 +0900

    send_function_from_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'memos#index'
-  resources :users, only: [:new, :create, :show]
-  resources :memos, only: [:index, :create, :show, :edit, :update, :destroy]
-  
-  # , only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:new, :show]
+  resources :memos, only: [:index, :create, :edit, :update, :destroy]
   
 end
